d824afd975265af1fc1d00810b3bc3e3eed978e5
berkozcan <berkhamzaozcan@gmail.com>
2022-12-06 22:57:30 +0100

    Proje dosyası ekle.

---
+++ MyStudentIdCard/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyStudentIdCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----MyStudentIdCard---------------");
+            Console.WriteLine("Name and Surname: BERK HAMZA OZCAN");
+            Console.WriteLine("Student ID Number:0055850");
+            Console.WriteLine("University:Vistula University");
+            Console.WriteLine("Country:Turkey");
+            Console.WriteLine("===================================");
+            Console.Read();
+        }
+    }
+}
